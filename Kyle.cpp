7dca8846896c93e0fcf54e6cbce3ce6eab349a3f
JulianG96 <31747304+JulianG96@users.noreply.github.com>
2017-09-07 15:13:23 -0700

    Update Kyle.cpp

--- Kyle.cpp
+++ Kyle.cpp
@@ -6,5 +6,6 @@ int main()
 	cout << "I am taking CSCI-150 class." << endl;
 	cout << "My name is Jiacheng Li." << endl;
 	cout << "This is new line." << endl;
+	cout << "My name is Julian." << endl;
 	return 0;
 }
