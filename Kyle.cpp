71bea586b86ad50ae76f178320452b2461bc2221
Kyle0775168 <31747315+Kyle0775168@users.noreply.github.com>
2017-09-07 15:11:21 -0700

    Update Kyle.cpp

--- Kyle.cpp
+++ Kyle.cpp
@@ -5,7 +5,6 @@ int main()
 {
 	cout << "I am taking CSCI-150 class." << endl;
 	cout << "My name is Jiacheng Li." << endl;
-	cout << "My name is Julian." << endl;
-	
+	cout << "This is new line." << endl;
 	return 0;
 }
