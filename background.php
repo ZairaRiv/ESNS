a8ce0a73260ec00e2b621c87b3679c81cf594e12
Agustin Rivera <agustin@rivera-web.com>
2017-11-18 20:48:51 +0000

    blah

--- background.php
+++ background.php
@@ -7,7 +7,7 @@
  */
 
 // You can't just do img_dot=img_orig.  Lame
-$img_orig = imagecreatefromjpeg('img/esnsbackground.jpg');
+$img_orig = imagecreatefromjpeg('img/esnsbackground.jpg?v=1');
 
 header('Content-Type: image/jpeg');
 imagejpeg($img_orig);
