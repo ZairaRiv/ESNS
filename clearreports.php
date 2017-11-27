ab60254d0743c2b5aa72103ee371a037048e47f8
Agustin Rivera <agustin@rivera-web.com>
2017-11-27 06:05:09 +0000

    Clear reports

---
+++ clearreports.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once ('phplibs/db.php');
+
+
+$data = new ESNSData();
+
+$data->ClearReports();
+echo "Reports Cleared<br>\n";
+
+//header("Location: /map.php?schoolID=$schoolID");
+
+
+
