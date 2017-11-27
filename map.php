1bf36dd652924929f5a8d4894af4a8e77441e080
Agustin Rivera <agustin@rivera-web.com>
2017-11-27 03:11:46 +0000

    Update to the map image.  Responsive resizing, user dot added, cropping added

--- map.php
+++ map.php
@@ -23,7 +23,7 @@ $schoolID = $_GET["schoolID"];
     <script>
         function getImage() {
             var userBuildingID=localStorage.getItem("userBuildingID");
-            var text = '<img src="mapimage.php?uID=' + userBuildingID + '">';
+            var text = '<img src="mapimage.php?uID=' + userBuildingID + '" class="fit-img">';
             return text;
         }
 
@@ -35,22 +35,56 @@ $schoolID = $_GET["schoolID"];
         }
     </script>
     <style>
-        img {
-            width: 100%;
-            height: auto;
-        }
+	    /* For this demo only */
+	    .container {
+		    max-height: 95%;
+		    margin: 0 auto;
+		    height: auto;
+	    }
+	    .img-frame {
+		    background: #fff;
+		    margin: 20px auto;
+	    }
+
+	    /* This is for responsive container with specified aspect ratio */
+	    .aspect-ratio {
+		    padding-bottom: 100%;
+	    }
+
+	    /* This is the key part - position and fit the image to the container */
+	    .fit-img {
+		    position: absolute;
+		    top: 0;
+		    right: 0;
+		    bottom: 0;
+		    left: 0;
+		    margin: auto;
+		    max-width: 100%;
+		    max-height: 100%
+	    }
     </style>
 </head>
 <body>
     <div style="text-align: center;">
-    <div id="main">
+    <!--div-- id="main">
         <div class="content">
             <div id="MAPIMAGE"></div>
         </div>
-        <?php
-            echo '<a href="report.php?schoolID=' . $schoolID . '">Make New Report</a>';
-        ?>
-    </div>
 
+    </div-->
+	    <?php
+	    echo '<a href="report.php?schoolID=' . $schoolID . '">Make New Report</a>';
+	    ?>
+	    <div class="container">
+
+		    <div class="aspect-ratio img-frame">
+			    <div id="MAPIMAGE">
+			    </div>
+		    </div>
+
+	    </div>
+
+
+    </div>
 </body>
 </html>
--- mapimage.php
+++ mapimage.php
@@ -6,8 +6,6 @@
  * Time: 3:39 PM
  */
 
-
-
 require_once ('phplibs/db.php');
 $data = new ESNSData();
 $perpBuildingID = $data->GetReports();
@@ -15,11 +13,21 @@ $buildingCount = array();
 $buildingNumbers = array();
 $percH = array();
 $percW = array();
-$radius = array();
+$cropPadding=10; // pixels
+$diameter = array();
+$userdiam=50;
+
+$uid= $_GET["uID"];
+
+// get user's buildingID info and add to array first
+$widthHeighth= $data->GetWidthHeight($uid);
+$whrow = $widthHeighth->fetch_assoc();
+$percH[$uid]=$whrow["percentHeight"];
+$percW[$uid]=$whrow["percentWidth"];
 
 while ($row = $perpBuildingID->fetch_assoc()) {
     $bid=$row["buildingShooterID"];
-	$radius[$bid]=$row["count(buildingShooterID)"];
+	$diameter[$bid]=$row["count(buildingShooterID)"];
 
 	// store the buildingID's so we can loop later without recalling the DB
     $buildingNumbers[$bid]=1;
@@ -39,21 +47,60 @@ while ($row = $perpBuildingID->fetch_assoc()) {
 
 // You can't just do img_dot=img_orig.  Lame
 $img_orig = imagecreatefromjpeg('/var/www/agustin/img/maps/csuf.jpg');
-$img_dot=imagecreatefromjpeg('/var/www/agustin/img/maps/csuf.jpg');
+$img_dot  = imagecreatefromjpeg('/var/www/agustin/img/maps/csuf.jpg');
+
+imagefilter($img_orig,IMG_FILTER_GRAYSCALE);
+imagefilter($img_dot,IMG_FILTER_GRAYSCALE);
+
+// set the max to 0, the min to max, guaranteeing they will be adjusted in the loop below
+$maxH=0;
+$maxW=0;
+$minH=imagesy($img_orig);
+$minW=imagesx($img_orig);
 
 foreach ($buildingNumbers as $bid => $notUseful) {
     $dotHeight=imagesy($img_orig)*$percH[$bid];
     $dotWidth =imagesx($img_orig)*$percW[$bid];
 
-    $radi = 50 + $radius[$bid]*10;
-    if ($radi>300) {
-        $radi=300; // size cap
+    $diam = 50 + $diameter[$bid]*10;
+	// also user's building, set minimum diameter to make sure it's visible under the user dot
+	if ($bid==$uid) {
+		if ($diam<100) {
+			$diam=100;
+		}
     }
 
-    $col_ellipse = imagecolorallocate($img_dot, 255, 0, 0);
-    imagefilledellipse($img_dot, $dotWidth, $dotHeight, $radi, $radi, $col_ellipse);
-}
+    if ($diam>200) {
+        $diam=200; // size cap
+    }
 
+	if ($dotHeight+$diam/2>$maxH) {
+		$maxH=$dotHeight+$diam/2;
+	}
+	if ($dotHeight-$diam/2<$minH) {
+		$minH=$dotHeight-$diam/2;
+	}
+	if ($dotWidth+$diam/2>$maxW) {
+		$maxW=$dotWidth+$diam/2;
+	}
+	if ($dotWidth-$diam/2<$minW) {
+		$minW=$dotWidth-$diam/2;
+	}
+
+	#echo "Dot $dotWidth x $dotHeight\n";
+	#echo "H: $minH => $maxH     W: $minW => $maxW\n\n";
+
+    $col_ellipse = imagecolorallocate($img_dot, 255, 0, 0);
+    imagefilledellipse($img_dot, $dotWidth, $dotHeight, $diam, $diam, $col_ellipse);
+}
+#echo "$minH $maxH $minW $maxW\n";
+
+
+// do user's building last to make sure the dot appears
+$dotHeight=imagesy($img_orig)*$percH[$uid];
+$dotWidth =imagesx($img_orig)*$percW[$uid];
+$col_ellipse = imagecolorallocate($img_dot, 0, 255, 0);
+imagefilledellipse($img_dot, $dotWidth, $dotHeight, $userdiam, $userdiam, $col_ellipse);
 
 // create blank canvas at merged_image
 $merged_image = imagecreatetruecolor(imagesx($img_orig), imagesy($img_orig));
@@ -67,9 +114,34 @@ imagecopy($merged_image,$img_orig,0,0,0,0,imagesx($img_orig),imagesy($img_orig))
 imagealphablending($merged_image, true);
 imagecopymerge($merged_image,$img_dot,0,0,0,0,imagesx($img_orig),imagesy($img_orig),50);
 
+
+// add some padding
+$minW -=$cropPadding;
+$maxW +=$cropPadding;
+$minH -=$cropPadding;
+$maxH +=$cropPadding;
+// make sure we don't crop beyond the image boundaries (resulting in an error)
+if ($minW<0) {
+	$minW=0;
+}
+if ($maxW>imagesx($img_orig)) {
+	$maxW=imagesx($img_orig);
+}
+if ($minH<0) {
+	$minH=0;
+}
+if ($maxH>imagesy($img_orig)) {
+	$maxH=imagesy($img_orig);
+}
+
+// crop image
+$croppedImage=imagecrop($merged_image, ['x'=>$minW, 'y'=>$minH, 'width'=>$maxW-$minW, 'height'=>$maxH-$minH]);
+
 header('Content-Type: image/gif');
 imagetruecolortopalette($merged_image, false, 64);
-imagegif($merged_image);
+imagegif($croppedImage);
+#imagegif($merged_image);
+imagedestroy($croppedImage);
 imagedestroy($merged_image);
 
 
